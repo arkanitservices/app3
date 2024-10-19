5846d9038ebd20be4456b2e5cb3cd40f0463b323
Hassan Asiri <hassanasiri@alMacBook-Air-alkhas-bHassan.local>
2024-10-19 13:12:21 +0300

    this frist change file

---
+++ android/app/src/main/kotlin/com/example/bitacaty_app33/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bitacaty_app33
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    home: Scaffold(
+    backgroundColor: const Color.fromARGB(138, 91, 191, 55
+    ),
+    ),
+    ),
+  }
+}
+}
